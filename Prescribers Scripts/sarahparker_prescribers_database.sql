7f413bdae30cb8a74f373e176ffb9c51febe3c49
sarahbelleparker <sarahbelleparker@outlook.com>
2024-02-16 16:12:36 -0600

    Finished Except Bonus

--- Prescribers Scripts/sarahparker_prescribers_database.sql
+++ Prescribers Scripts/sarahparker_prescribers_database.sql
@@ -126,3 +126,95 @@ GROUP BY CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
 	ELSE 'neither' END;
 	
 --Answer: More was spent on opioids than antibiotics 
+
+
+--5a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
+SELECT COUNT(cbsa)
+FROM cbsa
+	INNER JOIN fips_county ON cbsa.fipscounty = fips_county.fipscounty
+WHERE state = 'TN';
+
+--answer: There are 42 CBSAs in Tennessee.
+
+
+--5b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+SELECT cbsaname, SUM(population)
+FROM cbsa
+	INNER JOIN population ON population.fipscounty = cbsa.fipscounty
+GROUP BY cbsaname
+ORDER BY SUM(population) DESC;
+
+--answer: Nashville-Davidson-Murfreesboro-Franklin, TN is the largest CBSA with a total population of 1830410, and Morristown, TN is the smallest CBSA with a total population of 116352.
+
+
+--5c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+SELECT county, SUM(population)
+FROM fips_county
+	INNER JOIN population ON fips_county.fipscounty = population.fipscounty
+	LEFT JOIN cbsa ON population.fipscounty = cbsa.fipscounty
+WHERE cbsa IS NULL
+GROUP BY county
+ORDER BY SUM(population) DESC
+LIMIT 1;
+
+--answer: Sevier county has the largest population and is not in a CBSA
+
+
+--6a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+SELECT drug_name, total_claim_count
+FROM prescription
+WHERE total_claim_count > 2999;
+
+
+--6b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+SELECT prescription.drug_name, total_claim_count,
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid' ELSE 'other' END
+FROM prescription
+	INNER JOIN drug ON prescription.drug_name = drug.drug_name
+WHERE total_claim_count > 2999;
+
+
+--6c. Add another column to your answer from the previous part which gives the prescriber first and last name associated with each row.
+SELECT prescription.drug_name, total_claim_count,
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid' ELSE 'other' END, nppes_provider_first_name, nppes_provider_last_org_name
+FROM prescription
+	INNER JOIN drug ON prescription.drug_name = drug.drug_name
+	INNER JOIN prescriber ON prescription.npi = prescriber.npi
+WHERE total_claim_count > 2999
+ORDER BY total_claim_count DESC;
+
+
+--7a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) 
+--in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). 
+--**Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+SELECT npi, drug_name
+FROM prescriber
+	FULL JOIN drug ON 1 = 1
+WHERE specialty_description = 'Pain Management' AND
+	nppes_provider_city = 'NASHVILLE' AND
+	opioid_drug_flag = 'Y';
+	
+
+--7b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. 
+--You should report the npi, the drug name, and the number of claims (total_claim_count).
+SELECT prescriber.npi, drug.drug_name, SUM(total_claim_count)
+FROM prescriber
+	FULL JOIN drug ON 1 = 1
+	LEFT JOIN prescription ON prescription.npi = prescriber.npi AND drug.drug_name = prescription.drug_name
+WHERE specialty_description = 'Pain Management' AND
+	nppes_provider_city = 'NASHVILLE' AND
+	opioid_drug_flag = 'Y'
+GROUP BY prescriber.npi, drug.drug_name;
+
+
+--7c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+SELECT prescriber.npi, drug.drug_name, 
+	COALESCE(SUM(total_claim_count), 0)
+FROM prescriber
+	FULL JOIN drug ON 1 = 1
+	LEFT JOIN prescription ON prescription.npi = prescriber.npi AND drug.drug_name = prescription.drug_name
+WHERE specialty_description = 'Pain Management' AND
+	nppes_provider_city = 'NASHVILLE' AND
+	opioid_drug_flag = 'Y'
+GROUP BY prescriber.npi, drug.drug_name
+ORDER BY COALESCE(SUM(total_claim_count), 0) DESC;
